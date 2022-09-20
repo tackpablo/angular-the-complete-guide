ced0aab62b1781dc5e7a1f7c30444c6d9c561e33
Pablo <tackpablo@gmail.com>
2022-09-20 10:09:53 -0700

    section 2 combining all forms of databinding

--- 2 The Basics/template-app/src/app/servers/servers.component.ts
+++ 2 The Basics/template-app/src/app/servers/servers.component.ts
@@ -20,7 +20,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
