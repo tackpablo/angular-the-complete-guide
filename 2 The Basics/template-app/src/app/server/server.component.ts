49955a3db8dd44790cbebb2192b65cc2985f7388
Pablo <tackpablo@gmail.com>
2022-09-20 17:58:17 -0700

    section 2 styling elements dynamically with ngStyle

--- 2 The Basics/template-app/src/app/server/server.component.ts
+++ 2 The Basics/template-app/src/app/server/server.component.ts
@@ -8,6 +8,14 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
   getServerStatus() {
     return this.serverStatus;
   }
