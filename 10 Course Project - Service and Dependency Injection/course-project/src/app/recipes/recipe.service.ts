1915b2bed3ea0048c3be2ab957e267ea2af9d7a8
Pablo <tackpablo@gmail.com>
2022-09-27 16:26:57 -0700

    add ingredients to recipes

--- 10 Course Project - Service and Dependency Injection/course-project/src/app/recipes/recipe.service.ts
+++ 10 Course Project - Service and Dependency Injection/course-project/src/app/recipes/recipe.service.ts
@@ -7,14 +7,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "Tasty Schnitzel",
+      "A super-tasty Schnitzel - just awesome!",
+      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
     ),
     new Recipe(
-      "Another Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "Big Fat Burger",
+      "What else do you need to say?",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     ),
   ];
 
