f3c905eb4117d617aeafe5d2bdc8a9792101c388
Pablo <tackpablo@gmail.com>
2022-09-28 12:44:17 -0700

    section 11 redirecting and wildcard routes

--- 11 Changing Pages With Routing/routing/src/app/app.module.ts
+++ 11 Changing Pages With Routing/routing/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent }, // '/' route
@@ -29,6 +30,8 @@ const appRoutes: Routes = [
       { path: ":id/edit", component: EditServerComponent }, // '/servers/:id/edit' route
     ],
   }, // '/servers' route
+  { path: "not-found", component: PageNotFoundComponent },
+  { path: "**", redirectTo: "/not-found" },
 ];
 
 @NgModule({
@@ -40,6 +43,7 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
