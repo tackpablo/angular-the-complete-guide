18ad339717b8c439f4b507d4d6e0dbb7fb5c2b5f
Pablo <tackpablo@gmail.com>
2022-09-28 12:36:43 -0700

    section 11 add server edit, query param handling

--- 11 Changing Pages With Routing/routing/src/app/servers/server/server.component.ts
+++ 11 Changing Pages With Routing/routing/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { ServersService } from "../servers.service";
 
 @Component({
@@ -12,7 +12,8 @@ export class ServerComponent implements OnInit {
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -20,6 +21,13 @@ export class ServerComponent implements OnInit {
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params["id"]);
+    });
+  }
+
+  onEdit() {
+    this.router.navigate(["edit"], {
+      relativeTo: this.route,
+      queryParamsHandling: "preserve",
     });
   }
 }
