85efd51da313071d517387bed59c07a1ee20fedd
Pablo <tackpablo@gmail.com>
2022-09-29 14:39:48 -0700

    section 13 observable error and completion

--- 13 Understanding Observables/observables/src/app/home/home.component.ts
+++ 13 Understanding Observables/observables/src/app/home/home.component.ts
@@ -14,17 +14,34 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.frstObsSubscription = interval(1000).subscribe((count) => {
     //   console.log(count);
     // });
-    const customIntervalObservable = Observable.create((observer) => {
+    const customIntervalObservable = new Observable((observer) => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+
+        if (count === 5) {
+          observer.complete();
+        }
+
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!"));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed!");
+      }
+    );
   }
 
   ngOnDestroy() {
